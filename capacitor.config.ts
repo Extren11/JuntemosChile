9391044f4a536258036901018878dca56865c4a0
Bvnjjaaa <ben.romero@duocuc.cl>
2024-09-01 23:56:14 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'JuntemosChile',
+  webDir: 'www'
+};
+
+export default config;
